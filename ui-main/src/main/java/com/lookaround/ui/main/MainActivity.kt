1edaf601192cf71e5a21ceb4a9843f46c5c4930a
merengue <therealmerengue@gmail.com>
2020-11-22 18:21:44 +0100

    ui-main module

--- ui-main/src/main/java/com/lookaround/ui/main/MainActivity.kt
+++ ui-main/src/main/java/com/lookaround/ui/main/MainActivity.kt
@@ -1,7 +1,7 @@
-package com.lookaround
+package com.lookaround.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
