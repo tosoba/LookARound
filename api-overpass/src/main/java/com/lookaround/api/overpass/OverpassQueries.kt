d7b09e35263311affc0707f63ca4ef86c802cfcb
merengue <therealmerengue@gmail.com>
2020-11-28 18:43:51 +0100

    findPlacesOfType query

--- api-overpass/src/main/java/com/lookaround/api/overpass/OverpassQueries.kt
+++ api-overpass/src/main/java/com/lookaround/api/overpass/OverpassQueries.kt
@@ -6,28 +6,49 @@ import hu.supercluster.overpasser.library.output.OutputFormat
 import hu.supercluster.overpasser.library.output.OutputModificator
 import hu.supercluster.overpasser.library.output.OutputOrder
 import hu.supercluster.overpasser.library.output.OutputVerbosity
+import hu.supercluster.overpasser.library.query.OverpassFilterQuery
 import hu.supercluster.overpasser.library.query.OverpassQuery
+import kotlin.math.sqrt
 
 object OverpassQueries {
-    fun findNearbyAttractions(lat: Double, lng: Double, radiusInMeters: Long): String {
-        val southWest = SphericalUtil
-            .computeOffset(LatLng(lat, lng), radiusInMeters.toDouble(), 225.0)
-        val northEast = SphericalUtil
-            .computeOffset(LatLng(lat, lng), radiusInMeters.toDouble(), 45.0)
-        return OverpassQuery()
-            .format(OutputFormat.JSON)
-            .timeout(10)
-            .filterQuery()
-            .node()
-            .tag("tourism", "attraction")
-            .boundingBox(
-                southWest.latitude,
-                southWest.longitude,
-                northEast.latitude,
-                northEast.longitude
-            )
-            .end()
-            .output(OutputVerbosity.BODY, OutputModificator.CENTER, OutputOrder.QT, 100)
-            .build()
+    fun findAttractions(
+        lat: Double, lng: Double, radiusInMeters: Double
+    ): String = OverpassQuery()
+        .format(OutputFormat.JSON)
+        .timeout(10)
+        .filterQuery()
+        .node()
+        .tag("tourism", "attraction")
+        .boundingBox(lat, lng, radiusInMeters)
+        .end()
+        .output(OutputVerbosity.BODY, OutputModificator.CENTER, OutputOrder.QT, 100)
+        .build()
+
+    fun findPlacesOfType(
+        amenity: String, lat: Double, lng: Double, radiusInMeters: Double
+    ): String = OverpassQuery()
+        .format(OutputFormat.JSON)
+        .timeout(10)
+        .filterQuery()
+        .node()
+        .amenity(amenity)
+        .boundingBox(lat, lng, radiusInMeters)
+        .end()
+        .output(OutputVerbosity.BODY, OutputModificator.CENTER, OutputOrder.QT, 100)
+        .build()
+
+    private fun OverpassFilterQuery.boundingBox(
+        lat: Double, lng: Double, radiusInMeters: Double
+    ): OverpassFilterQuery {
+        val latLng = LatLng(lat, lng)
+        val distanceFromCenterToCorner = radiusInMeters * sqrt(2.0)
+        val southWest = SphericalUtil.computeOffset(latLng, distanceFromCenterToCorner, 225.0)
+        val northEast = SphericalUtil.computeOffset(latLng, distanceFromCenterToCorner, 45.0)
+        return boundingBox(
+            southWest.latitude,
+            southWest.longitude,
+            northEast.latitude,
+            northEast.longitude
+        )
     }
 }
