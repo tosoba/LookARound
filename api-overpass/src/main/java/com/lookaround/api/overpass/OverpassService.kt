a19242f4f76dd663240cf14c06dbe96b4168b0bd
Mere Ngue <therealmerengue@gmail.com>
2021-01-17 20:41:19 +0100

    Casting to Node.

--- api-overpass/src/main/java/com/lookaround/api/overpass/OverpassService.kt
+++ api-overpass/src/main/java/com/lookaround/api/overpass/OverpassService.kt
@@ -2,35 +2,39 @@ package com.lookaround.api.overpass
 
 import nice.fontaine.overpass.models.query.statements.NodeQuery
 import nice.fontaine.overpass.models.response.OverpassResponse
+import nice.fontaine.overpass.models.response.geometries.Node
 
 class OverpassService(private val endpoints: OverpassEndpoints) {
     suspend fun findAttractions(
         lat: Double, lng: Double, radiusInMeters: Float
-    ): OverpassResponse = endpoints.interpreter(
-        NodeQuery.Builder()
-            .equal("tourism", "attraction")
-            .around(lat, lng, radiusInMeters)
-            .build()
-            .toQuery()
-    )
+    ): List<Node> = nodesAround(lat, lng, radiusInMeters) { equal("tourism", "attraction") }
 
     suspend fun findPlacesOfType(
         type: String, lat: Double, lng: Double, radiusInMeters: Float
-    ): OverpassResponse = endpoints.interpreter(
-        NodeQuery.Builder()
-            .equal("amenity", type)
-            .around(lat, lng, radiusInMeters)
-            .build()
-            .toQuery()
-    )
+    ): List<Node> = nodesAround(lat, lng, radiusInMeters) { equal("amenity", type) }
 
     suspend fun findImages(
         lat: Double, lng: Double, radiusInMeters: Float
-    ): OverpassResponse = endpoints.interpreter(
-        NodeQuery.Builder()
-            .ilike("image", "http")
-            .around(lat, lng, radiusInMeters)
-            .build()
-            .toQuery()
-    )
+    ): List<String> = nodesAround(lat, lng, radiusInMeters) { ilike("image", "http") }
+        .mapNotNull { it.tags["image"] }
+
+    private suspend fun nodesAround(
+        lat: Double,
+        lng: Double,
+        radiusInMeters: Float,
+        compose: NodeQuery.Builder.() -> NodeQuery.Builder
+    ) = endpoints
+        .interpreter(NodeQuery.Builder().compose().aroundQuery(lat, lng, radiusInMeters))
+        .nodes
+
+    private fun NodeQuery.Builder.aroundQuery(
+        lat: Double, lng: Double, radiusInMeters: Float
+    ): String = around(lat, lng, radiusInMeters)
+        .build()
+        .toQuery()
+
+    private val OverpassResponse.nodes: List<Node>
+        get() = elements
+            .toList()
+            .filterIsInstance<Node>()
 }
--- api-overpass/src/test/java/com/lookaround/api/overpass/OverpassServiceTests.kt
+++ api-overpass/src/test/java/com/lookaround/api/overpass/OverpassServiceTests.kt
@@ -1,12 +1,18 @@
 package com.lookaround.api.overpass
 
+import com.squareup.moshi.Moshi
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import nice.fontaine.overpass.models.response.adapters.ElementAdapter
+import nice.fontaine.overpass.models.response.adapters.Iso8601Adapter
+import nice.fontaine.overpass.models.response.adapters.MemberAdapter
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.junit.Test
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.*
+
 
 @ExperimentalCoroutinesApi
 class OverpassServiceTests {
@@ -14,10 +20,16 @@ class OverpassServiceTests {
     private val warsawLng = 21.017532
     private val radius = 10_000.0f
 
+    private val moshi = Moshi.Builder()
+        .add(MemberAdapter())
+        .add(ElementAdapter())
+        .add(Date::class.java, Iso8601Adapter())
+        .build()
+
     private val service: OverpassService = OverpassService(
         Retrofit.Builder()
             .baseUrl(OverpassEndpoints.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .client(
                 OkHttpClient.Builder()
                     .addInterceptor(
