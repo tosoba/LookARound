0f4750577abada74a216487492ad4e820330ddde
Mere Ngue <therealmerengue@gmail.com>
2021-02-05 20:02:38 +0100

    More map scenes.

--- ui-map/src/main/java/com/lookaround/ui/map/MapScene.kt
+++ ui-map/src/main/java/com/lookaround/ui/map/MapScene.kt
@@ -1,5 +1,9 @@
 package com.lookaround.ui.map
 
 enum class MapScene(val url: String) {
-    BUBBLE_WRAP("https://www.nextzen.org/carto/bubble-wrap-style/9/bubble-wrap-style.zip")
+    BUBBLE_WRAP("https://www.nextzen.org/carto/bubble-wrap-style/9/bubble-wrap-style.zip"),
+    REFILL("https://www.nextzen.org/carto/refill-style/11/refill-style.zip"),
+    WALKABOUT("https://www.nextzen.org/carto/walkabout-style/7/walkabout-style.zip"),
+    TRON("https://www.nextzen.org/carto/tron-style/6/tron-style.zip"),
+    CINNABAR("https://www.nextzen.org/carto/cinnabar-style/9/cinnabar-style.zip")
 }
