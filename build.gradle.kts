621cd65197faa2e5d5a52895b48828e6ea0b21b3
merengue <therealmerengue@gmail.com>
2021-04-08 20:21:48 +0200

    gradle upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha12")
+        classpath("com.android.tools.build:gradle:7.0.0-alpha14")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.31.2-alpha")
     }
