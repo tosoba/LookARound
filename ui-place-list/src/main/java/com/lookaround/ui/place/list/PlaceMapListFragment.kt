534aa543d0e7363ab3bfe715c48b9d0cc6d9b440
merengue <therealmerengue@gmail.com>
2021-05-01 12:16:52 +0200

    processing places works (test version)

--- ui-place-list/src/main/java/com/lookaround/ui/place/list/PlaceMapListFragment.kt
+++ ui-place-list/src/main/java/com/lookaround/ui/place/list/PlaceMapListFragment.kt
@@ -8,26 +8,24 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.lookaround.core.android.ext.*
 import com.lookaround.core.android.map.MapScene
 import com.lookaround.core.android.model.WithValue
-import com.lookaround.core.delegate.lazyAsync
 import com.lookaround.ui.main.MainViewModel
 import com.lookaround.ui.place.list.databinding.FragmentPlaceMapListBinding
 import com.mapzen.tangram.*
 import com.mapzen.tangram.networking.HttpHandler
+import com.mapzen.tangram.viewholder.GLSurfaceViewHolderFactory
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.WithFragmentBindings
 import javax.inject.Inject
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 import timber.log.Timber
 
 @FlowPreview
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
 @WithFragmentBindings
-class PlaceMapListFragment :
-    Fragment(R.layout.fragment_place_map_list), MapController.SceneLoadListener {
-    private val binding: FragmentPlaceMapListBinding by viewBinding(
-        FragmentPlaceMapListBinding::bind
-    )
+class PlaceMapListFragment : Fragment(R.layout.fragment_place_map_list), MapChangeListener {
+    private val binding by viewBinding(FragmentPlaceMapListBinding::bind)
 
     @Inject internal lateinit var mainViewModelFactory: MainViewModel.Factory
     private val mainViewModel: MainViewModel by assistedActivityViewModel {
@@ -35,15 +33,22 @@ class PlaceMapListFragment :
     }
 
     @Inject internal lateinit var mapTilesHttpHandler: HttpHandler
-    private val mapController: Deferred<MapController> by lifecycleScope.lazyAsync {
-        binding.map.init(mapTilesHttpHandler)
-    }
+    private lateinit var mapController: MapController
+
+    private var processingPlaces: Boolean = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        mapController.launch {
-            setSceneLoadListener(this@PlaceMapListFragment)
-            loadScene(MapScene.BUBBLE_WRAP)
-        }
+        binding.map.getMapAsync(
+            { mc ->
+                mc?.let {
+                    mapController = it
+                    mapController.setMapChangeListener(this@PlaceMapListFragment)
+                    mapController.loadScene(MapScene.BUBBLE_WRAP)
+                }
+            },
+            GLSurfaceViewHolderFactory(),
+            mapTilesHttpHandler
+        )
     }
 
     override fun onDestroyView() {
@@ -66,54 +71,6 @@ class PlaceMapListFragment :
         binding.map.onLowMemory()
     }
 
-    override fun onSceneReady(sceneId: Int, sceneError: SceneError?) {
-        val markers = mainViewModel.state.markers as WithValue
-        markers.value[0].apply {
-            lifecycleScope.launch {
-                mapController.await().run {
-                    updateCameraPosition(
-                        CameraUpdateFactory.newCameraPosition(
-                            CameraPosition().apply {
-                                latitude = location.latitude
-                                longitude = location.longitude
-                            }
-                        )
-                    )
-                    captureFrame(
-                        { bitmap ->
-                            Timber.tag("BITM").e(bitmap.byteCount.toString())
-                            binding.captureImageView.setImageBitmap(bitmap)
-                        },
-                        true
-                    )
-                }
-            }
-        }
-        //        markers
-        //            .value
-        //            .items
-        //            .asFlow()
-        //            .take(1)
-        //            .onEach { marker ->
-        //                val location = marker.location
-        //                val bitmap =
-        //                    mapController.await().run {
-        //                        updateCameraPosition(
-        //                            CameraUpdateFactory.newCameraPosition(
-        //                                CameraPosition().apply {
-        //                                    latitude = location.latitude
-        //                                    longitude = location.longitude
-        //                                }
-        //                            )
-        //                        )
-        //                        captureFrame()
-        //                    }
-        //                Timber.tag("BIT").e(bitmap.byteCount.toString())
-        //                binding.captureImageView.setImageBitmap(bitmap)
-        //            }
-        //            .launchIn(lifecycleScope)
-    }
-
     private fun Deferred<MapController>.launch(block: suspend MapController.() -> Unit) {
         lifecycleScope.launch(Dispatchers.Main.immediate) { this@launch.await().block() }
     }
@@ -124,4 +81,34 @@ class PlaceMapListFragment :
             listOf(SceneUpdate("global.sdk_api_key", BuildConfig.NEXTZEN_API_KEY))
         )
     }
+
+    override fun onViewComplete() {
+        if (processingPlaces) return
+        processPlaces()
+        processingPlaces = true
+    }
+
+    private fun processPlaces() {
+        val markers = mainViewModel.state.markers as WithValue
+        lifecycleScope.launch {
+            markers.value.items.asFlow().collect { marker ->
+                val location = marker.location
+                mapController.updateCameraPosition(
+                    CameraUpdateFactory.newCameraPosition(
+                        CameraPosition().apply {
+                            latitude = location.latitude
+                            longitude = location.longitude
+                        }
+                    )
+                )
+                val bitmap = mapController.captureFrame(false)
+                Timber.tag("BIT").e(bitmap.byteCount.toString())
+                binding.captureImageView.setImageBitmap(bitmap)
+            }
+        }
+    }
+
+    override fun onRegionWillChange(p0: Boolean) = Unit
+    override fun onRegionIsChanging() = Unit
+    override fun onRegionDidChange(p0: Boolean) = Unit
 }
