822497240f7f6cdea626ce9517dfbbb7861c7674
merengue <therealmerengue@gmail.com>
2021-05-29 12:32:52 +0200

    two search result columns when horizontal

--- ui-search/src/main/java/com/lookaround/ui/search/composable/SearchResults.kt
+++ ui-search/src/main/java/com/lookaround/ui/search/composable/SearchResults.kt
@@ -1,5 +1,6 @@
 package com.lookaround.ui.search.composable
 
+import android.content.res.Configuration
 import android.location.Location
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import com.lookaround.core.android.ext.formattedDistanceTo
 import com.lookaround.core.android.model.Point
@@ -25,6 +27,7 @@ internal fun SearchResults(
     locationFlow: Flow<Location>,
     modifier: Modifier = Modifier
 ) {
+    val configuration = LocalConfiguration.current
     LazyColumn(modifier, verticalArrangement = Arrangement.spacedBy(10.dp)) {
         item { Spacer(modifier = Modifier.height(60.dp)) }
         if (!lastPerformedWithLocationPriority) {
@@ -39,37 +42,57 @@ internal fun SearchResults(
                 }
             }
         }
-        items(points) { point ->
-            SearchResultCard {
-                Column(modifier = Modifier.padding(5.dp)) {
-                    Text(
-                        text = point.name,
-                        style = MaterialTheme.typography.subtitle1,
-                        color = LookARoundTheme.colors.textPrimary,
-                        modifier = Modifier.heightIn(min = 20.dp).wrapContentHeight()
-                    )
-                    val location = locationFlow.collectAsState(null).value
-                    if (location != null) {
-                        Text(
-                            text = point.location.formattedDistanceTo(location),
-                            style = MaterialTheme.typography.subtitle2,
-                            color = LookARoundTheme.colors.textSecondary,
-                            modifier = Modifier.heightIn(min = 16.dp).wrapContentHeight()
-                        )
+        if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            items(points.chunked(2)) { chunk ->
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(10.dp),
+                    modifier = Modifier.wrapContentHeight()
+                ) {
+                    chunk.forEach { point ->
+                        SearchResult(point, locationFlow, Modifier.weight(1f))
                     }
                 }
+            }
+        } else {
+            items(points) { point -> SearchResult(point, locationFlow, Modifier.fillMaxWidth()) }
+        }
+    }
+}
+
+@Composable
+private fun SearchResult(
+    point: Point,
+    locationFlow: Flow<Location>,
+    modifier: Modifier = Modifier
+) {
+    SearchResultCard(modifier = modifier) {
+        Column(modifier = Modifier.padding(5.dp)) {
+            Text(
+                text = point.name,
+                style = MaterialTheme.typography.subtitle1,
+                color = LookARoundTheme.colors.textPrimary,
+                modifier = Modifier.heightIn(min = 20.dp).wrapContentHeight()
+            )
+            val location = locationFlow.collectAsState(null).value
+            if (location != null) {
+                Text(
+                    text = point.location.formattedDistanceTo(location),
+                    style = MaterialTheme.typography.subtitle2,
+                    color = LookARoundTheme.colors.textSecondary,
+                    modifier = Modifier.heightIn(min = 16.dp).wrapContentHeight()
+                )
             }
         }
     }
 }
 
 @Composable
-private fun SearchResultCard(content: @Composable () -> Unit) {
+private fun SearchResultCard(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
     Card(
         elevation = 4.dp,
         shape = RoundedCornerShape(12.dp),
         backgroundColor = Color.White,
-        modifier = Modifier.fillMaxWidth(),
+        modifier = modifier,
         content = content
     )
 }
