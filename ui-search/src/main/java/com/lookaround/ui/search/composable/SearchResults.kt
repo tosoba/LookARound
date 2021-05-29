c5961560e458a917479d46d9e3cbcdd60ca90f69
merengue <therealmerengue@gmail.com>
2021-05-29 11:35:19 +0200

    SearchResults changes cd.

--- ui-search/src/main/java/com/lookaround/ui/search/composable/SearchResults.kt
+++ ui-search/src/main/java/com/lookaround/ui/search/composable/SearchResults.kt
@@ -1,9 +1,6 @@
 package com.lookaround.ui.search.composable
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -12,22 +9,28 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.lookaround.core.android.model.Point
 import com.lookaround.core.android.view.theme.LookARoundTheme
 
 @Composable
 internal fun SearchResults(points: List<Point>, modifier: Modifier = Modifier) {
-    LazyColumn(modifier) {
+    LazyColumn(modifier, verticalArrangement = Arrangement.spacedBy(10.dp)) {
         items(points) { item ->
             // TODO: distance away from current location?
-            Card(elevation = 4.dp, shape = RoundedCornerShape(20.dp)) {
-                Column(modifier = Modifier.padding(10.dp)) {
+            Card(
+                elevation = 4.dp,
+                shape = RoundedCornerShape(12.dp),
+                backgroundColor = Color.White,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Column(modifier = Modifier.padding(5.dp)) {
                     Text(
                         text = item.name,
-                        style = MaterialTheme.typography.h6,
+                        style = MaterialTheme.typography.subtitle1,
                         color = LookARoundTheme.colors.textPrimary,
-                        modifier = Modifier.heightIn(min = 24.dp).wrapContentHeight()
+                        modifier = Modifier.heightIn(min = 20.dp).wrapContentHeight()
                     )
                 }
             }
