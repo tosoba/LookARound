b8dbf3101ab844f9407303de8713a3813b1c9da9
merengue <therealmerengue@gmail.com>
2021-10-02 12:22:09 +0200

    SampleMarkers coords change

--- ui-main/src/main/java/com/lookaround/ui/main/model/SampleMarkers.kt
+++ ui-main/src/main/java/com/lookaround/ui/main/model/SampleMarkers.kt
@@ -8,43 +8,43 @@ object SampleMarkers {
         listOf(
             Marker(
                 "KEN CENTER",
-                LocationFactory.create(52.153004, 21.040759, 30.0),
+                LocationFactory.create(52.253004, 20.940759, 30.0),
             ),
             Marker(
                 "SGGW",
-                LocationFactory.create(52.164851, 21.047542, 30.0),
+                LocationFactory.create(52.264851, 20.947542, 30.0),
             ),
             Marker(
                 "KEN CENTER",
-                LocationFactory.create(52.153005, 21.040761, 30.0),
+                LocationFactory.create(52.253005, 20.940761, 30.0),
             ),
             Marker(
                 "SGGW",
-                LocationFactory.create(52.164803, 21.047562, 30.0),
+                LocationFactory.create(52.264803, 20.947562, 30.0),
             ),
             Marker(
                 "KEN CENTER",
-                LocationFactory.create(52.153004, 21.040758, 30.0),
+                LocationFactory.create(52.253004, 20.940758, 30.0),
             ),
             Marker(
                 "SGGW",
-                LocationFactory.create(52.164804, 21.047563, 30.0),
+                LocationFactory.create(52.264804, 20.947563, 30.0),
             ),
             Marker(
                 "KEN CENTER",
-                LocationFactory.create(52.153006, 21.040756, 30.0),
+                LocationFactory.create(52.253006, 20.940756, 30.0),
             ),
             Marker(
                 "SGGW",
-                LocationFactory.create(52.164802, 21.047561, 30.0),
+                LocationFactory.create(52.264802, 20.947561, 30.0),
             ),
             Marker(
                 "KEN CENTER",
-                LocationFactory.create(52.153005, 21.040757, 30.0),
+                LocationFactory.create(52.253005, 20.940757, 30.0),
             ),
             Marker(
                 "SGGW",
-                LocationFactory.create(52.164801, 21.047560, 30.0),
+                LocationFactory.create(52.264801, 20.947560, 30.0),
             ),
         )
 }
