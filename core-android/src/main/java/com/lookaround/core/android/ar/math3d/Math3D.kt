184ba1b416a9e95d117db6f1d1a3f50cdeb2db88
merengue <therealmerengue@gmail.com>
2021-10-23 13:00:59 +0200

    factor in screen width in shouldDraw

--- core-android/src/main/java/com/lookaround/core/android/ar/math3d/Math3D.kt
+++ core-android/src/main/java/com/lookaround/core/android/ar/math3d/Math3D.kt
@@ -147,7 +147,7 @@ object Math3D {
                 inScreenSize.y / 2 +
                     viewPortPos.y * inScreenRotTrig.cos +
                     viewPortPos.x * inScreenRotTrig.sin
-            true
+            return outScreenPos.x >= 0.0 && outScreenPos.x <= inScreenSize.x
         } else {
             false
         }
