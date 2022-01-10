469ab30c78d1f76c07294137f056ce3ca8f4f38a
Tomasz Osoba <tomasz.osoba@comarch.pl>
2022-01-10 16:51:31 +0100

    removed unused ViewPager related stuff

--- core-android/src/main/java/com/lookaround/core/android/view/viewpager/ViewPagerFragmentFactory.kt
+++
@@ -1,8 +0,0 @@
-package com.lookaround.core.android.view.viewpager
-
-import androidx.fragment.app.Fragment
-
-interface ViewPagerFragmentFactory {
-    val newInstance: () -> Fragment
-    val fragmentId: Long
-}
