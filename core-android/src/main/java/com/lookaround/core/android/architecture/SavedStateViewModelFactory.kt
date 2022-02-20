63afe6ac9b9664adf8d389cef1b112769d4e03c3
merengue <therealmerengue@gmail.com>
2022-02-20 19:53:08 +0100

    removed unused VM factory

--- core-android/src/main/java/com/lookaround/core/android/architecture/SavedStateViewModelFactory.kt
+++
@@ -1,8 +0,0 @@
-package com.lookaround.core.android.architecture
-
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
-
-interface SavedStateViewModelFactory<VM : ViewModel> {
-    fun create(savedStateHandle: SavedStateHandle): VM
-}
