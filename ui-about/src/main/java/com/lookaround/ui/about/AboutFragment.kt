7820200a12b6c0107e1166a89e100242ca2f42ec
merengue <therealmerengue@gmail.com>
2022-05-21 10:38:35 +0200

    about fragment tabs

--- ui-about/src/main/java/com/lookaround/ui/about/AboutFragment.kt
+++ ui-about/src/main/java/com/lookaround/ui/about/AboutFragment.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.tabs.TabLayoutMediator
 import com.lookaround.ui.about.databinding.FragmentAboutBinding
 import com.lookaround.ui.main.MainViewModel
 import com.lookaround.ui.main.model.MainState
@@ -29,5 +31,31 @@ class AboutFragment : Fragment(R.layout.fragment_about) {
         }
 
         binding.aboutToolbar.setNavigationOnClickListener { activity?.onBackPressed() }
+
+        binding.aboutViewPager.adapter =
+            object : FragmentStateAdapter(requireActivity()) {
+                override fun getItemCount(): Int = 2
+
+                override fun createFragment(position: Int): Fragment =
+                    when (position) {
+                        0 -> GeneralFragment()
+                        1 -> DonateFragment()
+                        else -> throw IllegalArgumentException()
+                    }
+            }
+
+        TabLayoutMediator(binding.aboutTabLayout, binding.aboutViewPager) { tab, position ->
+                tab.text =
+                    when (position) {
+                        0 -> getString(R.string.general)
+                        1 -> getString(R.string.donate)
+                        else -> throw IllegalArgumentException()
+                    }
+            }
+            .attach()
     }
+
+    class GeneralFragment : Fragment(R.layout.fragment_general) {}
+
+    class DonateFragment : Fragment(R.layout.fragment_donate) {}
 }
